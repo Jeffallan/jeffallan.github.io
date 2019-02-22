4e6c5c9796f812ef691814f082ed70f39fa446fa
Jeff <dshostakovich30@gmail.com>
2019-02-22 02:12:02 -0600

    add about me and interests template

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 PY?=python3
 PELICAN?=pelican
 PELICANOPTS=
+THEME?=theme
 
 BASEDIR=$(CURDIR)
 INPUTDIR=$(BASEDIR)/content
@@ -41,42 +42,42 @@ help:
 	@echo '                                                                          '
 
 html:
-	$(PELICAN) $(INPUTDIR) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS)
+	$(PELICAN) $(INPUTDIR) -o $(OUTPUTDIR) -t $(THEME) -s $(CONFFILE) $(PELICANOPTS)
 
 clean:
 	[ ! -d $(OUTPUTDIR) ] || rm -rf $(OUTPUTDIR)
 
 regenerate:
-	$(PELICAN) -r $(INPUTDIR) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS)
+	$(PELICAN) -r $(INPUTDIR) -o $(OUTPUTDIR) -t $(THEME) -s $(CONFFILE) $(PELICANOPTS)
 
 serve:
 ifdef PORT
-	$(PELICAN) -l $(INPUTDIR) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS) -p $(PORT)
+	$(PELICAN) -l $(INPUTDIR) -o $(OUTPUTDIR) -t $(THEME) -s $(CONFFILE) $(PELICANOPTS) -p $(PORT)
 else
-	$(PELICAN) -l $(INPUTDIR) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS)
+	$(PELICAN) -l $(INPUTDIR) -o $(OUTPUTDIR) -t $(THEME) -s $(CONFFILE) $(PELICANOPTS)
 endif
 
 serve-global:
 ifdef SERVER
-	$(PELICAN) -l $(INPUTDIR) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS) -p $(PORT) -b $(SERVER)
+	$(PELICAN) -l $(INPUTDIR) -o $(OUTPUTDIR) -t $(THEME) -s $(CONFFILE) $(PELICANOPTS) -p $(PORT) -b $(SERVER)
 else
-	$(PELICAN) -l $(INPUTDIR) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS) -p $(PORT) -b 0.0.0.0
+	$(PELICAN) -l $(INPUTDIR) -o $(OUTPUTDIR) -t $(THEME) -s $(CONFFILE) $(PELICANOPTS) -p $(PORT) -b 0.0.0.0
 endif
 
 
 devserver:
 ifdef PORT
-	$(PELICAN) -lr $(INPUTDIR) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS) -p $(PORT)
+	$(PELICAN) -lr $(INPUTDIR) -t $(THEME) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS) -p $(PORT)
 else
-	$(PELICAN) -lr $(INPUTDIR) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS)
+	$(PELICAN) -lr $(INPUTDIR) -t $(THEME) -o $(OUTPUTDIR) -s $(CONFFILE) $(PELICANOPTS)
 endif
 
 publish:
-	$(PELICAN) $(INPUTDIR) -o $(OUTPUTDIR) -s $(PUBLISHCONF) $(PELICANOPTS)
+	$(PELICAN) $(INPUTDIR) -o $(OUTPUTDIR) -t $(THEME) -s $(PUBLISHCONF) $(PELICANOPTS)
 
 github: publish
 	ghp-import -m "Generate Pelican site" -b $(GITHUB_PAGES_BRANCH) $(OUTPUTDIR)
-	git push origin $(GITHUB_PAGES_BRANCH)
+	git push Github $(GITHUB_PAGES_BRANCH)
 
 
 .PHONY: html help clean regenerate serve serve-global devserver stopserver publish github
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Jeff Smolinski'
 SITENAME = "Jeff's Site"
-SITEURL = ''
+SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
 
@@ -32,4 +32,30 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+ABOUT_ME = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis placerat imperdiet lectus, in commodo dui. Curabitur euismod arcu elit, in pellentesque nunc pulvinar maximus. Nulla ornare tempus urna, non interdum urna aliquam suscipit. Nunc hendrerit, arcu cursus commodo semper, nibh sapien egestas quam, tempor elementum lacus metus non enim. Nunc suscipit augue risus, ut aliquet ligula porttitor in. Praesent nisi mi, sagittis ac tincidunt in, mollis interdum ipsum. Nullam lacus nunc, mattis ut quam eget, porta bibendum metus. Pellentesque molestie elit eget nibh mollis dapibus. Praesent vulputate posuere sagittis. In vitae volutpat velit.
+
+Praesent aliquet facilisis magna. Vivamus sit amet lorem dapibus nisl hendrerit blandit. Sed vestibulum iaculis massa, vitae vehicula mi vehicula commodo. Maecenas faucibus, nisi at lacinia tincidunt, dui diam congue erat, placerat lobortis sem purus vel quam. Quisque commodo justo porta lacus vulputate, eu ultricies magna egestas. Fusce mattis orci vel eros pulvinar, hendrerit aliquam ex vestibulum. Maecenas ac ipsum a lacus tincidunt iaculis. Nunc suscipit diam vitae enim elementum, eu ornare sapien posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+
+"""
+
+HIGHLIGHTS = ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+'Phasellus non eros sed risus tempor iaculis.'
+'Sed volutpat nisl vel rhoncus dapibus.',
+'Ut lacinia tortor porta, elementum enim eget, hendrerit turpis.',
+'Curabitur sollicitudin ligula in quam sodales, at lacinia metus commodo.')
+
+INTERESTS = """"""
+
+PROJECTS = ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+'Phasellus non eros sed risus tempor iaculis.'
+'Sed volutpat nisl vel rhoncus dapibus.',
+'Ut lacinia tortor porta, elementum enim eget, hendrerit turpis.',
+'Curabitur sollicitudin ligula in quam sodales, at lacinia metus commodo.')
+
+PRESENTATIONS = ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+'Phasellus non eros sed risus tempor iaculis.'
+'Sed volutpat nisl vel rhoncus dapibus.',
+'Ut lacinia tortor porta, elementum enim eget, hendrerit turpis.',
+'Curabitur sollicitudin ligula in quam sodales, at lacinia metus commodo.')
