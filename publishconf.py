e9b7ab18f3a66f1b0035f6956c4089bfbf4e84d9
Jeff <dshostakovich30@gmail.com>
2019-02-24 00:53:53 -0600

    remopve atom feed from publish config

--- publishconf.py
+++ publishconf.py
@@ -17,8 +17,8 @@ from pelicanconf import *
 SITEURL = 'https://jeffallan.github.io/'
 RELATIVE_URLS = True
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
